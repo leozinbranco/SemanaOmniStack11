f5690ec385255a0221601bd5d7178098e246a79a
Leonardo <cyndrasilgames@gmail.com>
2020-03-28 19:59:54 -0300

    Mobile Application

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import intl from 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
